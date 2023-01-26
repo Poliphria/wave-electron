fb1a559c637bde6e4f66611549f6185450acf216
Christian Moronta <cmoronta26@gmail.com>
2023-01-26 01:18:15 -0500

    Add home screen text.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
-import {
-  VStack,
-  Text,
-  Flex,
-} from "@chakra-ui/react";
-import Container from "./individual/Container";
-import YoutubeLink from "./individual/YouTubeLink";
-import UploadButton from "./individual/UploadButton"
-import ColorModeSwitcher from "./individual/ColorModeSwitcher";
-
+import { VStack, Text, Flex, useColorModeValue } from '@chakra-ui/react';
+import Container from './individual/Container';
+import YoutubeLink from './individual/YouTubeLink';
+import UploadButton from './individual/UploadButton';
+import ColorModeSwitcher from './individual/ColorModeSwitcher';
+import '@fontsource/anton';
 
 export default function Home() {
+  const textGradient = useColorModeValue(
+    'linear-gradient(to right, #ee0979, #ff6a00)',
+    'linear-gradient(to right, #e96443, #904e95)'
+  );
   return (
     <Container justifyContent="center" fontSize="xl">
       <VStack
@@ -20,25 +20,22 @@ export default function Home() {
         justify="center"
         flexGrow="4"
       >
-        <UploadButton />
-        <Text>
-          Or
+        <Text
+          fontFamily="anton"
+          fontSize="6rem"
+          letterSpacing="0.4rem"
+          bg={textGradient}
+          backgroundClip="text"
+        >
+          WAVE
         </Text>
+        <UploadButton />
+        <Text>Or</Text>
         <YoutubeLink />
       </VStack>
-      <Flex
-        justify="flex-end"
-        width="100%"
-        flexGrow="1"
-      >
-        <ColorModeSwitcher
-          alignSelf="flex-end"
-          mr="1rem"
-          mb="1rem"
-        />
+      <Flex justify="flex-end" width="100%" flexGrow="1">
+        <ColorModeSwitcher alignSelf="flex-end" mr="1rem" mb="1rem" />
       </Flex>
-
     </Container>
-  )
-
+  );
 }
