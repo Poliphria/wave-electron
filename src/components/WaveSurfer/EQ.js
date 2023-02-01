e31cb2e8925dbb56a477e1d68e8752f54cef673e
Christian Moronta <cmoronta26@gmail.com>
2023-02-01 04:18:37 -0500

    Add reset EQ value button functionality.

--- src/components/WaveSurfer/EQ.js
+++ src/components/WaveSurfer/EQ.js
@@ -1,22 +1,49 @@
-import { Text, Flex, Box } from '@chakra-ui/react';
+import { Text, Flex, Box, Button } from '@chakra-ui/react';
+import { useState } from 'react';
 import EQSlider from './EQSlider';
 
-const EQ = ({ wavesurferRef, filters }) => {
-  console.log('FROM EQ: ', wavesurferRef);
+const EQ = ({ filters }) => {
+  const [sliderValues, setSliderValues] = useState(
+    new Array(filters.length).fill(0)
+  );
+
+  console.log('slidervalues: ', sliderValues);
+
+  const handleResetButtonClick = () => {
+    filters.forEach(filter => {
+      filter.gain.value = 0;
+    });
+    setSliderValues(new Array(filters.length).fill(0));
+  };
+
+  const handleChange = (index, value) => {
+    setSliderValues(prev => {
+      const sliders = [...prev];
+      sliders[index] = value;
+      return sliders;
+    });
+  };
 
   let filterSliders = filters.map((filter, index) => {
     return (
       <Box key={index}>
-        <EQSlider filter={filter} />
+        <EQSlider
+          filter={filter}
+          filterIndex={index}
+          handleChange={handleChange}
+        />
       </Box>
     );
   });
 
   return (
     <Flex width="100%" justify="space-around" pt={4}>
-      <Text flexShrink="2" justifySelf="flex-start" alignSelf="flex-end">
-        Hz
-      </Text>
+      <Flex flexDir="column" justifyContent="space-between">
+        <Button onClick={handleResetButtonClick}>Reset</Button>
+        <Text flexShrink="2" alignSelf="flex-end">
+          Hz
+        </Text>
+      </Flex>
       {filterSliders}
     </Flex>
   );
--- src/components/WaveSurfer/EQSlider.js
+++ src/components/WaveSurfer/EQSlider.js
@@ -6,19 +6,13 @@ import {
   SliderThumb,
   Text,
 } from '@chakra-ui/react';
-import { useState } from 'react';
 const MAX_SLIDER_VALUE = 40;
 const MIN_SLIDER_VALUE = -40;
 
-const EQSlider = props => {
-  const [sliderValue, setSliderValue] = useState(0);
-
-  const handleChange = value => {
-    setSliderValue(value);
-    props.filter.gain.value = Math.floor(value);
-    console.log(
-      `Current filter (${props.filter.frequency.value}) value: ${props.filter.gain.value}`
-    );
+const EQSlider = ({ filter, handleChange, filterIndex }) => {
+  const customChange = value => {
+    filter.gain.value = Math.round(value);
+    handleChange(filterIndex, value);
   };
   return (
     <Flex flexDir="column">
@@ -29,14 +23,15 @@ const EQSlider = props => {
         minH="48"
         max={MAX_SLIDER_VALUE}
         min={MIN_SLIDER_VALUE}
-        onChange={handleChange}
+        onChange={customChange}
+        value={filter.gain.value}
       >
         <SliderTrack>
           <SliderFilledTrack />
         </SliderTrack>
         <SliderThumb />
       </Slider>
-      <Text pt={2}>{props.filter.frequency.value}</Text>
+      <Text pt={2}>{filter.frequency.value}</Text>
     </Flex>
   );
 };
