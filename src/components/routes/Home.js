65e36731160dded14a7e84cebae21660df5e5da7
Christian Moronta <cmoronta26@gmail.com>
2023-02-01 19:42:46 -0500

    Add home title text (got deleted on merge)

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,10 +1,15 @@
-import { VStack, Text, Flex } from '@chakra-ui/react';
+import { VStack, Text, Flex, useColorModeValue } from '@chakra-ui/react';
 import Container from '../individual/Container';
 import YoutubeLink from '../individual/YouTubeLink';
 import UploadButton from '../individual/UploadButton';
 import ColorModeSwitcher from '../individual/ColorModeSwitcher';
+import '@fontsource/anton';
 
 export default function Home() {
+  const textGradient = useColorModeValue(
+    'linear-gradient(to right, #ee0979, #ff6a00)',
+    'linear-gradient(to right, #e96443, #904e95)'
+  );
   return (
     <Container justifyContent="center" fontSize="xl">
       <VStack
@@ -15,6 +20,15 @@ export default function Home() {
         justify="center"
         flexGrow="4"
       >
+        <Text
+          fontFamily="anton"
+          fontSize="6rem"
+          letterSpacing="0.4rem"
+          bg={textGradient}
+          backgroundClip="text"
+        >
+          WAVE
+        </Text>
         <UploadButton />
         <Text>Or</Text>
         <YoutubeLink />
